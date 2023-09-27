651210f396fc4ee337b2e3f0207b6e77fdb245e8
Rudra Arora <aroraru@tcd.ie>
2023-09-27 17:50:07 +0100

    main setup with clap

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,17 @@ pub mod cmd;
 use clap::Parser;
 use cmd::CliCommand;
 
-macro_rules! print_typewriter {
-    ($text:expr,$color:expr, $delay:expr) => {
-        print!("\x1b[ q");
-        for c in $text.chars() {
-            print!("{}", c.to_string().color($color).bold());
-            io::stdout().flush().unwrap();
-            thread::sleep($delay);
-        }
-        print!("\x1b[ q");
-    };
-}
+// macro_rules! print_typewriter {
+//     ($text:expr,$color:expr, $delay:expr) => {
+//         print!("\x1b[ q");
+//         for c in $text.chars() {
+//             print!("{}", c.to_string().color($color).bold());
+//             io::stdout().flush().unwrap();
+//             thread::sleep($delay);
+//         }
+//         print!("\x1b[ q");
+//     };
+// }
 
 #[derive(Parser, Debug)]
 struct XCommands {
